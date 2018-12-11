b3fac6e391cb638ce27b50bef9c8e74d5d8a5250
long.le <xuanlongvts@gmail.com>
2018-12-11 15:15:12 +0700

    iota

---
+++ 03_exercise_level_2/03/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const (
+	x     = 42
+	y int = 100
+)
+
+/*
+	or
+	const x = 42
+	const y int = 100
+*/
+
+func main() {
+	fmt.Println(x, y)
+}
---
+++ 03_exercise_level_2/05/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := `test sfj
+	ds
+	fsdfsd 
+	sfjfjlsafs
+	`
+
+	fmt.Println(str)
+}
