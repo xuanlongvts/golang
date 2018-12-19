b40a2d638953c39a7f8ca49ec43b930043f29532
long.le <xuanlongvts@gmail.com>
2018-12-19 14:17:02 +0700

    interface

--- 09_functions/06_interface_polymorphism/DienTichTronChuNhat/interfaceDemo.go
+++ 09_functions/06_interface_polymorphism/DienTichTronChuNhat/interfaceDemo.go
@@ -57,3 +57,23 @@ func main() {
 	totalT := totalArea(&c, &r)
 	fmt.Println("total=", totalT) // total
 }
+
+/*
+Trong các ví dụ trên, chúng ta đã định nghĩa 2 struct là Rectangle (hình chữ nhật) và Circle (hình tròn),
+cả 2 struct này đều một phương thức tính diện tích có tên giống nhau là area().
+Chúng ta có thể “gộp chung” 2 phương thức đó vào một kiểu dữ liệu khác có tên là Interface:
+
+Trong ví dụ trên chúng ta định nghĩa một Interface có tên Shape, interface này có một phương thức là area().
+Để định nghĩa một interface thì cũng giống như định nghĩa một struct, chúng ta dùng từ khóa type,
+tiếp đến là tên interface rồi đến từ khóa interface, sau đó là danh sách các phương thức trong cặp dấu ngoặc nhọn {}.
+
+Interface thực ra cũng không hẳn là một kiểu dữ liệu như struct vì interface chỉ chứa các phương thức chứ không chứa các trường,
+interface cũng không có phần định nghĩa phương thức ở ngoài như các struct, chúng chỉ chứa tên phương thức là hết.
+Vậy thì việc sử dụng interface có gì hay? Câu trả lời là chúng ta có thể dùng interface để thực hiện tính toán trên nhiều kiểu struct
+khác nhau mà không quan tâm các struct đó là gì.
+
+Trong ví dụ trên, chúng ta định nghĩa hàm totalArea() có chức năng tính tổng diện tích của bất cứ hình nào, hàm này nhận vào tham số là kiểu Shape,
+nhưng chúng ta có thể truyền vào kiểu Circle hoặc kiểu Rectangle đều được, nếu chúng ta truyền vào kiểu Circle,
+khi gọi phương thức area() thì Go sẽ gọi phương thức area() của struct Circle, và ngược lại khi truyền vào Rectangle thì gọi phương thức area()
+của Rectangle.
+*/
