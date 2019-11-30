592ea3132834a1b879d2e1daeed74840a797bf45
LongLe <v.longlx4@vinid.net>
2019-11-30 16:35:19 +0700

    project part 2

--- projects-demo/pkg/server/main.go
+++ projects-demo/pkg/server/main.go
@@ -7,16 +7,38 @@ import (
 	"github.com/cmelgarejo/go-gql-server/pkg/utils"
 )
 
-var HOST, PORT string
+var host, port, gqlPath, gqlPgPath string
+var isPgEnabled bool
 
 func init() {
-	HOST = utils.MustGet("GQL_SERVER_HOST")
-	PORT = utils.MustGet("GQL_SERVER_PORT")
+	host = utils.MustGet("GQL_SERVER_HOST")
+	port = utils.MustGet("GQL_SERVER_PORT")
+	gqlPath = utils.MustGet("GQL_SERVER_GRAPHQL_PATH")
+	gqlPgPath = utils.MustGet("GQL_SERVER_GRAPHQL_PLAYGROUND_PATH")
+	isPgEnabled = utils.MustGetBool("GQL_SERVER_GRAPHQL_PLAYGROUND_ENABLED")
 }
 
 func Run() {
+	endpoint := "http://" + host + ":" + port
+
 	r := gin.Default()
+
+	// Handlers
+	// Simple keep-alive/ping handler
 	r.GET("/ping", handlers.Ping())
-	log.Println("Running @ http://" + HOST + ":" + PORT )
-	log.Fatalln(r.Run(HOST + ":" + PORT))
+
+	// GraphQL handlers
+	// Playground handler
+	if isPgEnabled {
+		r.GET(gqlPgPath, handlers.PlaygroundHandler(gqlPath))
+		log.Println("GraphQL Playground @ " + endpoint + gqlPgPath)
+	}
+	r.POST(gqlPath, handlers.GraphqlHandler())
+	log.Println("GraphQL @ " + endpoint + gqlPath)
+
+	// Run the server
+	// Inform the user where the server is listening
+	log.Println("Running @ " + endpoint)
+	// Print out and exit(1) to the OS if the server cannot run
+	log.Fatalln(r.Run(host + ":" + port))
 }
