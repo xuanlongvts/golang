b3ad2eae4c1e612c2c279a1d325486ea1d1fe9b1
longlx <longlx@hasaki.vn>
2023-02-23 10:50:10 +0700

    files interact

---
+++ with-enviroment/utils/config.go
@@ -0,0 +1,7 @@
+package utils
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
