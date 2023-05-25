dbaeaad4f5f290941c7326e0fff0676d7a674c49
longlx <longlx@hasaki.vn>
2023-05-25 09:49:38 +0700

    decodeRFC2047

--- stuffs/decodeRFC2047/main.go
+++ stuffs/decodeRFC2047/main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/jhillyerd/enmime"
+	"mime"
 	"strings"
 )
 
 func main() {
 	//encodedFilename := "=?UTF-8?B?SFNLLVAtMDIzX1BMMDUgSMaw4bubbmcgZOG6q24ga2hhaSBiw6FvIHdvcmtmbG93ICgxKS5wZGY=?="
 	//encodedFilename := "=?UTF-8?B?SFNLLVAtMDIzX1BMMDUgSMaw4bubbmcgZOG6q24ga2hhaSBiw6FvLnBkZg==?="
-	//
-	//decodedFilename := enmime.DecodeRFC2047(encodedFilename)
-	//
-	//fmt.Println("Decoded Filename:", decodedFilename)
+	encodedFilename := "=?UTF-8?B?SFNLLUhELTAzMiAtIEjGsOG7m25nIGThuqtuIHPhu60gZOG7pW5nIERhaWx5IFRhc2sgdOG6oWkgSA==?= =?UTF-8?B?YXNha2kgV29yayAtIFZlci4gMDAucGRm?="
 
+	decoder := new(mime.WordDecoder)
+	decodedFilename, err := decoder.Decode(encodedFilename)
+	if err != nil {
+		fmt.Println("Error decoding filename:", err)
+	}
+	fmt.Println("mime Filename:", decodedFilename)
+
+	decodedFilename = enmime.DecodeRFC2047(encodedFilename)
+	fmt.Println("enmime Filename:", decodedFilename)
+
+	fmt.Println("---------------------------")
 	var str = "name:  attachment; filename=\"=?UTF-8?B?SFNLLVAtMDIzX1BMMDUgSMaw4bubbmcgZOG6q24ga2hhaSBiw6FvLnBkZg==?=\""
 	res1 := strings.Split(str, "\"")
-
 	fmt.Println("res1: ", res1[1])
 }
