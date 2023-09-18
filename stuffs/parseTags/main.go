6c73c4f36e19870e49246d957c84c6e20bbebef8
longlx <longlx@hasaki.vn>
2023-09-18 08:54:14 +0700

    link hashtag return done

--- stuffs/parseTags/main.go
+++ stuffs/parseTags/main.go
@@ -6,27 +6,29 @@ import (
 	"strings"
 )
 
-func parseTag(strData string) (listLinks []string, listHashtag []string) {
+func parseTag(strData string) (listHashtag []string) {
 	var regexLink = `(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?`
 	var regexTagText = `(#+[a-zA-Z0-9(_)]{1,})`
 
 	reLink := regexp.MustCompile(regexLink)
 	listLinResks := reLink.FindAllString(strData, -1)
 
+	var newStrData string
 	for _, v := range listLinResks {
-		aaa := strings.Replace(strData, v, "", -1)
-		fmt.Println("aaa: ", aaa)
+		newStrData = strings.Replace(strData, v, "", -1)
+		strData = newStrData
 	}
 
 	reTagText := regexp.MustCompile(regexTagText)
 	reTagTextRes := reTagText.FindAllString(strData, -1)
 
-	return listLinResks, reTagTextRes
+	return reTagTextRes
 }
 
 func main() {
 	strData := " #fff #sdf https://php-play.dev/?c=DwfgDgFmBQAkBOBTAzgVw#DYBcAEBebYAhvMo#gCqEDm https://genk.vn/sdfds"
-	listLinks, listHashtag := parseTag(strData)
-	fmt.Println("listLinks: ", listLinks)
+	listHashtag := parseTag(strData)
+
+	fmt.Println("--------------")
 	fmt.Println("listHashtag: ", listHashtag)
 }
