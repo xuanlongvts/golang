9a845a6c1ed9eba1861c38e0b4dd3adc0c5ed2a2
longlx <xuanlongvts@gmail.com>
2023-12-07 10:42:44 +0700

    match short expression

--- stuffs/parseUserNameId/main.go
+++ stuffs/parseUserNameId/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"strings"
 )
 
 func main() {
@@ -11,14 +10,8 @@ func main() {
 	str := `abc test testing edited @[Nguyễn Nhật Lam](user_id:1198)  @[Nguyễn Văn Lam](user_id:1199)`
 
 	arr, _ := regexp.Compile(`(?im)@\[([^\]]+?)\]\(user_id:([^\]]+?)\)`)
-	for _, match := range arr.FindAllString(str, -1) {
+	for _, match := range arr.FindAllStringSubmatch(str, -1) {
 		fmt.Println("--------------------")
-		splStr := strings.Split(match, "@[")[1]
-		getFullName := strings.Split(splStr, "](user_id:")[0]
-		fmt.Println(getFullName)
-
-		getID := strings.Split(splStr, "](user_id:")[1]
-		getID = strings.Split(getID, ")")[0]
-		fmt.Println(getID)
+		fmt.Println(match[1], match[2])
 	}
 }
